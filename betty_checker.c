0f1f7c4fdab9cd9c7901c96c1658ed052755d1ff
Gfrim <gfrim79@gmail.com>
2022-08-24 02:44:23 -0700

    Updated

--- betty_checker.c
+++ betty_checker.c
@@ -13,14 +13,15 @@
 int main(void)
 {
 	int num1, num2, sum;
-	pid_t pid;
+	pid_t pid, ppid;
 
 	num1 = 3;
 	num2 = 7;
 	sum = num1 + num2;
 	pid = getpid();
+	ppid = getppid();
 
-	printf("Sum = %d\nPID = %u\n", sum, pid);
+	printf("Sum = %d\nPID = %u\nPPID = %u\n", sum, pid, ppid);
 	return (0);
 }
 
